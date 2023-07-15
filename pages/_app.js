8b023cc9315fc38b0f4d351194503efa8443d64b
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-07-16 00:33:08 +0530

    edit title

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,14 @@ import '../styles/globals.css';
 const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
-      <title>Metaversus</title>
+      <title>Metaversus - Abhishek</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://stijndv.com" />
-      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+      <link
+        rel="stylesheet"
+        href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+      />
     </Head>
     <Component {...pageProps} />
   </>
